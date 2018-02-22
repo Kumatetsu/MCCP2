ba675140074042fcae029723fb9d13e028eaeae5
Jean BILLAUD <billau_j@etna-alternance.net>
2018-02-22 16:25:27 +0100

    modifications

--- api/routes/availabilities.js
+++ api/routes/availabilities.js
@@ -1,4 +1,5 @@
-const resContract = require('../contract.js');
+const resContract = require('../contract.js'),
+      Web3        = require('web3');
 
 var availabilityContract;
 resContract.deployed().then(function (instance){
@@ -14,11 +15,11 @@ module.exports = function (app) {
 
             for (i = 0; i < length; i++) {
                 var tmp = {
-                    'key' : result[0][i].c[0],
-                    'provider': result[1][i],
-                    'booker': result[2][i],
-                    'type' : result[4][i].c[0],
-                    'resourceId': result[3][i].c[0],
+                    'key' :            result[0][i].c[0],
+                    'provider':        result[1][i],
+                    'booker':          result[2][i],
+                    'type' :           result[4][i].c[0],
+                    'resourceId':      result[3][i].c[0],
                     'bookingStatuses': result[5][i].c[0]
                 };
                 contracts.push(tmp);
@@ -31,10 +32,22 @@ module.exports = function (app) {
         })
     });
 
-    app.get('/listAvailabilityDetails', function(req, res) {
-        availabilityContract.listAvailabilityDetails(0)
+    app.get('/availability/:nbr', function(req, res) {
+        var id = req.params.nbr;
+
+        availabilityContract.listAvailabilityDetails(id)
         .then( (result) => {
-            res.send(result);
+            console.log(Web3);
+            var availability = {
+                'id': result[0].c[0],
+                'min_deposit': result[1].c[0],
+                'commission': result[2].c[0],
+                'freeCancelDate': result[3].c[0],
+                'bookingStatus': result[4].c[0],
+                'metaDataLink': Web3.utils.toUtf8(result[5])
+            };
+            console.log (availability);
+            res.send(availability);
         })
         .catch ((err) => {
             console.log(err);
